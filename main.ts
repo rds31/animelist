4803327a601397dc057ec4154a19e3a1dfefb2e3
rds31 <lightyear1603@gmail.com>
2026-06-29 15:09:51 +0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ app.use(staticFiles());
 
 // this is the same as the /api/:name route defined via a file. feel free to delete this!
 app.get("/api2/:name", (ctx) => {
-  const name = ctx.params.name;
+  const name = "Lightyear1603";
   return new Response(
     `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
   );
