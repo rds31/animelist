6df389b787105f6a08e3561ff707a9525ef042b0
rds31 <lightyear1603@gmail.com>
2026-06-29 15:07:42 +0600

    Update [name].tsx

--- routes/api/[name].tsx
+++ routes/api/[name].tsx
@@ -2,7 +2,7 @@ import { define } from "../../utils.ts";
 
 export const handler = define.handlers({
   GET(ctx) {
-    const name = Lightyear1603;
+    const name = "Lightyear1603";
     return new Response(
       `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
     );
